f89d1226dd2736c6062d40f974a58c231eff871e
TAYSIIR-ALRAWEE <tetteysir@gmail.com>
2024-07-26 23:29:39 +0300

    Create quiz.py

---
+++ quiz.py
@@ -0,0 +1,20 @@
+import math
+
+# Öklid mesafesini hesaplayan fonksiyon
+def euclideanDistance(point1, point2):
+    return math.sqrt((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2)
+
+# Noktaların tanımlanması
+points = [(1, 2), (3, 4), (5, 6), (7, 8), (2, 1)]
+
+# Mesafelerin hesaplanması
+distances = []
+for i in range(len(points)):
+    for j in range(i + 1, len(points)):
+        dist = euclideanDistance(points[i], points[j])
+        distances.append(dist)
+
+# Minimum mesafenin bulunması
+min_distance = min(distances)
+
+print("Noktalar arasındaki minimum Öklid mesafesi:", min_distance)
